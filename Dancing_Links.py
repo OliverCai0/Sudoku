964d9fc437febc1312fb10f34d2d4dbfeeff61a4
Oliver <ocai00@stuy.edu>
2020-06-21 20:34:02 -0400

    added methods

--- Dancing_Links.py
+++ Dancing_Links.py
@@ -1,81 +1,64 @@
-import math
-
 class Node():
 
-    def __init__(self,right,left,up,down,head,val):
+    def __init__(self,right,left,up,down,head):
         self.right = right
         self.left = left
         self.up = up
         self.down = down
         self.head = head
-        #matrix initialization purposes
-        self.val = val
-
-    def get_right(self):
-        return self.right
-
-    def get_left(self):
-        return self.left
-
-    def get_up(self):
-        return self.up
-
-    def get_down(self):
-        return self.down
-
-    def get_head(self):
-        return self.head
 
 
 class Col_Head(Node):
 
-    def __init__(self,right,left,up,down,head,val,size,name):
+    def __init__(self,right,left,up,down,head,size,name):
         Node.__init__(self,right,left,up,down,head)
         self.size = size
         self.name = name
 
-
-def create_initial_matrix(matrix):
-
-    #input matrix col,row ---> made of 0s and 1s
-    #column headers --> string names for requirements
-
-    head = Node(None,None,None,None,None,None)
-    curnode = head
-
-    #establishng up and down links
-    for c in range(len(matrix)):
-        #column header initialization
-        curnode.right = Col_Head(head,curnode,None,None,None,None,0,matrix[c][0])
-        curnode = curnode.right
-        c_head = curnode.right
-        for r in range(1,len(matrix[c])):
-            curnode.down = Node(None,None,curnode,c_head,c_head,matrix[c][r])
-            if matrix[c][r] == 1:
-                c_head.size += 1
-            if c == 0:
-                c.
-            curnode = curnode.down
-        curnode = curnode.head
-    curnode = head
-
-    while curnode.right != head:
-        curnode = curnode.right
-        c_head = curnode
-        while curnode.down != c_head:
-            curnode = curnode.down
-            start_node = curnode
-            while curnode.right != start_node:
-                curnode = curnode.right
-
-        curnode = curnode.head
+def initialize_constraints(constraints):
+    head = Node(None,None,None,None,None)
+    head.right = head
+    head.left = head
+    for c in range(len(constraints)):
+        if c == 0:
+            curnode = Col_Head(None,None,None,None,None,0,constraints[c])
+            head.right = curnode
+            head.right.left = head
+        else:
+            curnode.right = Col_Head(None,curnode,None,None,None,0,constraints[c])
+            curnode = curnode.right
+        if c == len(constraints) - 1:
+            curnode.right = head
+            curnode.right.left = curnode
+    return head
+        
+def add_row(head,row):
+    #row is givn in 1..1... string format
+    cur_col = head
+    nodes_added = []
+    for i in range(len(row)):
+        cur_col = cur_col.right
+        if row[i] == '1':
+            if not cur_col.down:
+                cur_col.down = Node(None,None,cur_col,cur_col,cur_col)
+                cur_col.up = cur_col.down
+                nodes_added.append(cur_col.down)
+            else:
+                cur_col.up.down = Node(None,None,cur_col.up,cur_col,cur_col)
+                cur_col.up = cur_col.up.down
+                nodes_added.append(cur_col.up)
+            cur_col.size += 1
+    for i in range(len(nodes_added)):
+        if i == 0:
+            nodes_added[i].left = nodes_added[-1]
+            nodes_added[i].left.right = nodes_added[i]
+        else:
+            nodes_added[i].left = nodes_added[i - 1]
+            nodes_added[i].left.right = nodes_added[i]
         
 
-    return head
 
-
-
-def search(k):
+def search(head):
 
     pass
 
@@ -101,3 +84,11 @@ def uncover(col):
     col.right.left = col
     col.left.right = col
 
+def print_dlx(head):
+    cols = head
+    col_count = 0
+    
+    while cols.right != head:
+        print(cols.right.name + ' : ' + cols.right.size)
+        col_count += 1
+        cols = cols.right
